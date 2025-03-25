3b1c8fef7319996d121c0e8911c9f1145c3fcfdd
Josiah Guevarra <701031808@GoTVDSB.ca>
2025-03-25 14:42:00 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
+
+namespace summitive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            double winnings, bet;
+            string rock, paper, scissors;
+
+
+
+            winnings = 50;
+            
+            
+           while (winnings == 0)
+            {
+                Console.WriteLine("you currently have $" + winnings);
+                Console.WriteLine("how much would you like to bet?");
+                Console.ReadLine();
+                bet = Convert.ToDouble(Console.ReadLine());
+                if (bet == 0 || bet < 0|| bet > winnings)
+                {
+                    Console.WriteLine("Bet invalid, please try again")
+                }
+
+
+            }
+            
+
+            
+            
+
+           
+            
+        }
+    }
+}
