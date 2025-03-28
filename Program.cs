73e9dbcb8679d3371916ca97782023ef7722b8d7
Josiah Guevarra <701031808@GoTVDSB.ca>
2025-03-28 14:00:58 -0400

    finished

--- Program.cs
+++ Program.cs
@@ -10,18 +10,18 @@ namespace summitive
     {
         static void Main(string[] args)
         {
-
-            int rock, paper, scissors,ai;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            int aiRand;
             double winnings, bet;
-            string userPick;
+            string userPick,ai,again;
             bool done = false;
-            rock = 1;
-            paper = 2;
-            scissors = 3;
+            
             Random generator = new Random();
             winnings = 50;
 
-
+            Console.Beep(500, 1000);
             Console.WriteLine("Welcome to Josiah's cassino!!!");
             Console.WriteLine();
             Console.WriteLine("We will be playing Rock,Paper,Scissors!");
@@ -29,9 +29,9 @@ namespace summitive
             while (!done)
             {
                 Console.WriteLine();
-                Console.WriteLine("you currently have $" + winnings);
+                Console.WriteLine("You currently have $" + winnings);
                 Console.WriteLine();
-                Console.WriteLine("how much would you like to bet?");
+                Console.WriteLine("How much would you like to bet?");
                 Console.WriteLine();
                 double.TryParse(Console.ReadLine(), out bet);
 
@@ -42,7 +42,7 @@ namespace summitive
                     Console.WriteLine("Invalid bet, please try again");
                     Console.ForegroundColor = ConsoleColor.White;
                     double.TryParse(Console.ReadLine(), out bet);
-                    Console.Clear();
+                    
 
                 }
                 Console.Clear();
@@ -52,28 +52,91 @@ namespace summitive
                 Console.WriteLine();
                 userPick = Console.ReadLine().ToLower();
 
-                if (userPick != "rock" || userPick != "paper" || userPick != "scissors")
+                while  (userPick != "rock" && userPick != "paper" && userPick != "scissors")
                 {
                     Console.ForegroundColor= ConsoleColor.Red;
                     Console.WriteLine("Invaild input");
-                    Console.WriteLine("please make sure you pick rock paper or scissors");
+                    Console.WriteLine("Please make sure you pick rock paper or scissors");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.Clear();
+                    Console.WriteLine();
+                    userPick = Console.ReadLine().ToLower();
                 }
-                ai = generator.Next(1, 4);
+                aiRand = generator.Next(1, 4);
+                if (aiRand == 1)
+                    ai = "rock";
+                else if (aiRand == 2)
+                    ai = "paper";
+                else  ai = "scissors";
                 
+                Console.WriteLine("The house picked " + ai);
+                if (userPick == ai)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine();
+                    Console.WriteLine("You tied, you keep your money");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                }
+                else if
+                    ((userPick == "rock" && ai == "scissors") ||
+                    (userPick == "paper" && ai == "rock") ||
+                    (userPick == "scissors" && ai == "paper"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("You WON!");
+                    winnings += bet;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You lost");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    winnings -= bet; 
+                }
+
+
+
+                if (winnings <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You have no more money, you lose");
+                    Console.WriteLine("Thanks for playing");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine("Would you like to play again?");
+                    Console.WriteLine("yes or no");
+                    again = Console.ReadLine().ToLower();
+                    if (again == "yes" || again == "y")
+                    {
+                        done = false;
+                        Console.Clear();
+                    }
+                    else
+                        
+                    if (again == "no"|| again == "n") 
+                    {
+                        done = true;
+                        Console.WriteLine("Thanks for playing");
+                        Console.ReadLine();
+                    }
+                }
+
 
             }
-            
 
-            
-             
 
-            
-            
 
-           
-            
+
+
+
+
+
+
+
         }
     }
 }
